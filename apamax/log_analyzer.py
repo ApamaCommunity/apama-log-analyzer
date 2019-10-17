41b38358086beadd4d76d8f94cd5a54ff7da579c
ben-spiller <ben-spiller@users.noreply.github.com>
2019-10-18 00:37:58 +0100

    bump version to 3.0

--- apamax/log_analyzer.py
+++ apamax/log_analyzer.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
-""" This is a script for analyzing Apama correlator (and apama-ctrl) log files. 
+""" This is a Python 3 script for analyzing Apama correlator (and apama-ctrl) log files. 
 
-It extracts and summarizes information from status lines and other 
-log messages. 
+It extracts and summarizes information from status lines and other log messages.
+
+
+Copyright (c) 2019 Software AG, Darmstadt, Germany and/or its licensors
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+file except in compliance with the License. You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. 
+See the License for the specific language governing permissions and limitations under the License.
+
+These tools are provided as-is and without warranty or support. They do not constitute part of the Software AG product suite. Users are free to use, fork and modify them, subject to the license agreement. 
+
 """
 
-__version__ = '3.0.dev'
-__date__ = '2019-10-14'
+__version__ = '3.0'
+__date__ = '2019-10-18'
 __author__ = "Apama community"
 __license__ = "Apache 2.0"
 
