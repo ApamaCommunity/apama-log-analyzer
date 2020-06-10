1df40e02ad1c84d2b3231893f1887e2a60eca74b
ben-spiller <ben-spiller@users.noreply.github.com>
2020-06-10 17:45:02 +0100

    generate graphs in a time that includes DST (e.g. BST) to show up a potential bug

--- tests/correctness/Cor_017/run.py
+++ tests/correctness/Cor_017/run.py
@@ -6,14 +6,14 @@ class PySysTest(AnalyzerBaseTest):
 	statuslines = 1000
 	def execute(self):
 		
-		self.copy(self.input+'/startup.log', 'logfile1.log', mappers=[lambda l: l.replace('2019-01-01 ', '2019-01-05 ')])
-		self.copy(self.input+'/startup.log', 'logfile2.log', mappers=[lambda l: l.replace('2019-01-01 ', '2019-01-06 ')])
+		self.copy(self.input+'/startup.log', 'logfile1.log', mappers=[lambda l: l.replace('2019-06-01 ', '2019-06-05 ')])
+		self.copy(self.input+'/startup.log', 'logfile2.log', mappers=[lambda l: l.replace('2019-06-01 ', '2019-06-06 ')])
 
 		for logfile in [1, 2, 3]:
 			logperiod = 60 if logfile==1 else 5 # status lines every minute to get a more interesting time range
 			with io.open(self.output+f'/logfile{logfile}.log', 'a', encoding='utf-8') as f:
 				if logfile != 3: # for #3 let it jut follow on from #2
-					starttime = calendar.timegm(time.strptime(f'2019-01-0{5+logfile-1} 10:06:01', '%Y-%m-%d %H:%M:%S'))
+					starttime = calendar.timegm(time.strptime(f'2019-06-0{5+logfile-1} 10:06:01', '%Y-%m-%d %H:%M:%S'))
 				rx = tx = 0
 				rnd = random.Random(logfile)
 				ceiling = {
@@ -89,7 +89,7 @@ class PySysTest(AnalyzerBaseTest):
 					f.write(f"{timestamp}.888 INFO  [22872] - Correlator Status: sm={d['sm']:.0f} nctx={d['nctx']:.0f} ls={d['ls']:.0f} rq={d['rq']:.0f} iq={d['iq']:.0f} oq={d['oq']:.0f} icq={d['icq']:.0f} lcn=\"<none>\" lcq=0 lct=0.0 rx={rx:.0f} tx={tx:.0f} rt=0 nc={d['nc']:.0f} vm=22580 pm={d['pm']:.0f} runq={d['runq']:.0f} si={d['si']:0.1f} so={d['so']:0.1f} srn=\"<none>\" srq=0 jvm={d['jvm']:.0f}\n")
 
 		# example of one with a long name, and which has some non-ID characters
-		log3 = 'logfile3.2019-01-01_02.04.011_hostname.dnsname.com_my@$correlator_logfile.log'
+		log3 = 'logfile3.2019-06-01_02.04.011_hostname.dnsname.com_my@$correlator_logfile.log'
 		os.rename(self.output+'/logfile3.log', self.output+'/'+log3)
 		self.logAnalyzer([], logfiles=[
 			self.output+'/logfile1.log',
@@ -111,7 +111,7 @@ class PySysTest(AnalyzerBaseTest):
 		self.assertGrep('loganalyzer_output/overview.html', expr=r'defaultCorrelator<1>', contains=False)
 		
 		# escaping of @$ characters in chart ids
-		self.assertGrep('loganalyzer_output/overview.html', expr=r'chart_[a-z]*_logfile3.2019-01-01_02.04.011_hostname.dnsname.com_my__correlator_logfile')
+		self.assertGrep('loganalyzer_output/overview.html', expr=r'chart_[a-z]*_logfile3.2019-06-01_02.04.011_hostname.dnsname.com_my__correlator_logfile')
 		
 		# includes actual apama version in the section on what to do when filing a support case
 		self.assertGrep('loganalyzer_output/overview.html', expr=r'<li><span class="key">Apama version: </span>10.5.0.0.123456</li>')
